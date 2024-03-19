5926e0a875f058e34aa73d2c81274cf662f5f277
shahmoksh22 <mokshpshah22@gmail.com>
2024-03-19 23:51:20 +0530

    Lab Work 8.5 & 8.6
    Lab Work 8.5 & 8.6

--- ui_demo/lib/LabWork8.5&8.6.dart
+++ ui_demo/lib/LabWork8.5&8.6.dart
@@ -1,6 +1,11 @@
+// Please Import Ecommerce_Custom_Container.dart & bottom_right_text.dart
+// and Assets Images To Run This Code
+// Made By Shahmoksh22
+
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:ui_demo/Ecommerce_Custom_Container.dart';
+import 'package:ui_demo/bottom_right_text.dart';
 
 class Screen85 extends StatefulWidget {
   const Screen85({Key? key}) : super(key: key);
@@ -26,129 +31,133 @@ class _Screen85State extends State<Screen85> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.all(20),
-              child: DropdownButton<String>(
-                value: _selectedItem,
-                onChanged: (newValue) {
-                  setState(() {
-                    _selectedItem = newValue!;
-                  });
-                },
-                items: <String>['Select Category', 'Mobile', 'Laptop', 'TV', 'PC']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
+      body: Stack(
+        children: [SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                child: DropdownButton<String>(
+                  value: _selectedItem,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _selectedItem = newValue!;
+                    });
+                  },
+                  items: <String>['Select Category', 'Mobile', 'Laptop', 'TV', 'PC']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Mobiles",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold
-              ),),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/Iphone15ProMax.jpg',
-                      productName: 'iPhone 15 Pro Max',
-                      productPrice: '\$ 549',
-                      productDiscount: '15.79',
-                      initialRating: 5,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/Iphone15.jpg',
-                      productName: 'iPhone 15',
-                      productPrice: '\$ 449',
-                      productDiscount: '10.59',
-                      initialRating: 4,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/Iphone15Plus.jpg',
-                      productName: 'iPhone 15 Plus',
-                      productPrice: '\$ 499',
-                      productDiscount: '7.59',
-                      initialRating: 4.5,
-                    ),
-                  ),
-                      ],
-                    ),
-                  ),
-                 SizedBox(
-                   height: 10,
-                   width: double.infinity,
-                 ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Laptop",
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Mobiles",
                 style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
                 ),),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0), // Add margin around the CustomProductContainer
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/MacBookM3.jpg',
-                      productName: 'MacBook M3 Chip',
-                      productPrice: '\$ 999',
-                      productDiscount: '10.59',
-                      initialRating: 5,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/AsusTuf.jpg',
-                      productName: 'Asus Tuf Laptop',
-                      productPrice: '\$ 779',
-                      productDiscount: '8.56',
-                      initialRating: 4,
-                    ),
-                  ),Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CustomProductContainer(
-                      imagePath: 'assets/images/HPVICTUS.jpg',
-                      productName: 'Hp Victus Laptop',
-                      productPrice: '\$ 570',
-                      productDiscount: '4.54',
-                      initialRating: 4.5,
-                    ),
-                  ),
-                ],
               ),
-            ),
-            SizedBox(
-              height: 10,
-              width: double.infinity,
-            )
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/Iphone15ProMax.jpg',
+                        productName: 'iPhone 15 Pro Max',
+                        productPrice: '\$ 549',
+                        productDiscount: '15.79',
+                        initialRating: 5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/Iphone15.jpg',
+                        productName: 'iPhone 15',
+                        productPrice: '\$ 449',
+                        productDiscount: '10.59',
+                        initialRating: 4,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/Iphone15Plus.jpg',
+                        productName: 'iPhone 15 Plus',
+                        productPrice: '\$ 499',
+                        productDiscount: '7.59',
+                        initialRating: 4.5,
+                      ),
+                    ),
+                        ],
+                      ),
+                    ),
+                   SizedBox(
+                     height: 10,
+                     width: double.infinity,
+                   ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Laptop",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(8.0), // Add margin around the CustomProductContainer
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/MacBookM3.jpg',
+                        productName: 'MacBook M3 Chip',
+                        productPrice: '\$ 999',
+                        productDiscount: '10.59',
+                        initialRating: 5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/AsusTuf.jpg',
+                        productName: 'Asus Tuf Laptop',
+                        productPrice: '\$ 779',
+                        productDiscount: '8.56',
+                        initialRating: 4,
+                      ),
+                    ),Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomProductContainer(
+                        imagePath: 'assets/images/HPVICTUS.jpg',
+                        productName: 'Hp Victus Laptop',
+                        productPrice: '\$ 570',
+                        productDiscount: '4.54',
+                        initialRating: 4.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+                width: double.infinity,
+              )
 
-                ],
-              ),
+                  ],
+                ),
+        ),
+          BottomRightText(),
+      ]
       ),
           );
   }
